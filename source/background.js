f6655461fdca519e291eed7943a0aa5e11a03997
jymsy <jym20083617@163.com>
2016-06-14 21:35:25 +0800

    init

---
+++ source/background.js
@@ -0,0 +1,235 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+if (!chrome.cookies) {
+  chrome.cookies = chrome.experimental.cookies;
+}
+
+var url = 'https://swgjazz.ibm.com:8017/jazz/service/com.ibm.team.workitem.common.internal.rest.IQueryRestService/getResultSet';
+var post_field = 'startIndex=0&maxResults=10&filterAttribute=&filterValue=&itemId=';
+var refer = 'https://swgjazz.ibm.com:8017/jazz/web/projects/Social%20CRM%20-%20Sales%20Force%20Automation';
+var isActivated = false;
+var isInitialized=false;
+var item_url = 'https://swgjazz.ibm.com:8017/jazz/web/projects/Social%20CRM%20-%20Sales%20Force%20Automation#action=com.ibm.team.workitem.viewWorkItem&id=';
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        if (details.type === 'xmlhttprequest') {
+          details.requestHeaders.push({ name: 'Referer', value: refer});
+          return { requestHeaders: details.requestHeaders };
+        }
+    },
+    {urls: ['https://swgjazz.ibm.com:8017/*']},
+    ["blocking", "requestHeaders"]
+);
+
+//right button click
+chrome.contextMenus.create({
+    title: "Add to Focusing On",
+    documentUrlPatterns: ["https://swgjazz.ibm.com:8017/*"],
+    onclick: function (info, tab){
+      var id = info.pageUrl.match(/.*id=(\d+$)/)[1];
+      if (id) {
+        var item = {id:id, summary:tab.title};
+        localStorageArrayAppend('focusingOn', item);
+        chrome.runtime.sendMessage({
+          type: "addFocusingOn",
+          value: item
+        });
+        //53
+      }
+
+    }
+});
+
+function localStorageArrayAppend(key, item) {
+  if (item) {
+    var current = JSON.parse(localStorage.getItem(key));
+    if (current) {
+      current.push(item);
+      localStorage.setItem(key, JSON.stringify(current));
+    }
+  };
+}
+
+/*
+  Displays a notification with the current time. Requires "notifications"
+  permission in the manifest file (or calling
+  "Notification.requestPermission" beforehand).
+*/
+function show(body,id) {
+
+  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
+  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+
+  var opt = {
+    type: "list",
+    title: hour + time[2] + ' ' + period,
+    message: "Primary message to display",
+    iconUrl: "48.png",
+    items: body,
+    buttons: [{title:'去看看'}]
+  };
+  chrome.notifications.create((new Date()).valueOf()+':'+id, opt, function(notificationId){
+    console.log('show notification');
+  });
+}
+
+function showNotice(value, date){
+  var body = [];
+        body.push({title:"ID", message:value['labels'][1]});
+        body.push({title:"Summary", message:value['labels'][2]});
+        body.push({title:"Priority & Severity", message:value['labels'][5] + ' | '+value['labels'][6]});
+
+        date = new Date(date).toLocaleString();
+        body.push({title:"Modified Date", message:date});
+        show(body, value['labels'][1]);
+}
+
+function updateLastModifyDate(id, newDate) {
+    var currentFilter = JSON.parse(localStorage.filter);
+    currentFilter.forEach(function(filter) {
+        if (filter.id == id) {
+            filter.lastModifyDate = newDate;
+        }
+    });
+    localStorage.filter=JSON.stringify(currentFilter);
+}
+
+function parseResultList(result, filter) {
+    var items = result['soapenv:Body']['response']['returnValue']['value']['rows'];
+    console.log(items);
+    var lastDate=0;
+    items.forEach(function(value, index) {
+      var date = parseInt(value['labels'][7]);
+      if (filter.lastModifyDate == 1 || date > filter.lastModifyDate) {
+        if (date > lastDate) {
+            lastDate = date;
+        }
+
+        var todayItems = JSON.parse(sessionStorage.todayItems);
+        var newItem = {
+          id: value['labels'][1],
+          summary:value['labels'][2]
+        };
+
+        var message = {
+          type: 'addNewItem',
+          value: newItem
+        };
+
+        todayItems.push(newItem);
+        sessionStorage.todayItems = JSON.stringify(todayItems);
+
+        chrome.runtime.sendMessage(message);
+
+        showNotice(value, date);
+      }
+    });
+    if (lastDate != 0) {
+      localStorage.lastItemDate = lastDate;
+      updateLastModifyDate(filter.id, lastDate);
+    }
+}
+
+function getRTCList(cookies) {
+  if (!localStorage.lastItemDate) {
+    localStorage.lastItemDate = 1;
+  }
+  var currentFilter = JSON.parse(localStorage.filter);
+
+  currentFilter.forEach(function(filter) {
+    var xmlhttp = new XMLHttpRequest();
+    var result;
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+        parseResultList(JSON.parse(xmlhttp.responseText), filter);
+      }
+    }
+    xmlhttp.open("POST",url,true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded; charset=UTF-8");
+    xmlhttp.setRequestHeader("accept","text/json");
+    xmlhttp.send(post_field+filter.id);
+  });
+
+}
+
+function onload() {
+  chrome.cookies.getAll({domain:'swgjazz.ibm.com'}, function(cookies) {
+    // startListening();
+    console.log(cookies);
+    // Test for notification support.
+    if (window.Notification) {
+      // While activated, show notifications at the display frequency.
+      if (isActivated) { getRTCList(cookies); }
+
+      var interval = 0; // The display interval, in seconds.
+
+      setInterval(function() {
+        interval++;
+        // console.log(interval);
+        if (isActivated && localStorage.frequency <= interval) {
+            interval = 0;
+            getRTCList(cookies);
+        }
+      }, 1000);
+    }    
+  });
+  // show('error!login rtc error! please login rtc again');
+}
+
+chrome.notifications.onClicked.addListener(function(notificationId){
+  chrome.notifications.clear(notificationId);
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    if (message.type === "initItems") {
+        sendResponse(JSON.parse(sessionStorage.todayItems));
+    }
+});
+
+chrome.notifications.onButtonClicked.addListener(function(notificationId,buttonIndex){
+  chrome.notifications.clear(notificationId);
+  var id = /.*:(.*)/.exec(notificationId)[1];
+  chrome.tabs.create({url:item_url+id});
+});
+
+if (!localStorage.frequency) {
+  localStorage.frequency = 60;
+}
+
+if (!localStorage.filter) {
+  var defaultFilter = [
+    {name:"Copy of 2.2 Unresolved Defects - Found in R3.1",id:"_Rxcb4I8rEeSN-dPMeJF_tQ", lastModifyDate:1}
+  ];
+  localStorage.filter=JSON.stringify(defaultFilter);
+}
+
+if(!sessionStorage.todayItems) {
+  sessionStorage.todayItems = JSON.stringify([]);
+}
+
+if(!localStorage.focusingOn) {
+  localStorage.focusingOn = JSON.stringify([]);
+}
+
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    sendResponse({}); // snub them.
+    if (request.cmd == 'start' && !isActivated) {
+      isActivated = true;
+      if (!isInitialized) {
+        isInitialized = true;
+        onload();
+      }
+    } else if (request.cmd == 'stop'){
+      isActivated = false;
+    }
+    console.log('get request:'+request.cmd);
+    
+    
+  }
+);
+
+
---
+++ source/build/html.js
@@ -0,0 +1,281 @@
+var item_url = 'https://swgjazz.ibm.com:8017/jazz/web/projects/Social%20CRM%20-%20Sales%20Force%20Automation#action=com.ibm.team.workitem.viewWorkItem&id=';
+var reloadFilterEvt = 'reload Filter';
+
+var FilterRow = React.createClass({
+  handleClick: function() {
+    this.props.onDeleted(this.props.item);
+  },
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.item.name}</td>
+        <td>{this.props.item.id}</td>
+        <td><button onClick={this.handleClick} className="delete_filter btn btn-danger" >Delete</button></td>
+      </tr>
+    );
+  }
+});
+
+var CurrentFilters = React.createClass({
+  getInitialState: function() {
+    var currentFilterList = JSON.parse(localStorage.filter);
+    return {
+      list: currentFilterList
+    };
+  },
+  componentDidMount: function() {
+    PubSub.subscribe(reloadFilterEvt, function(){
+      var currentFilterList = JSON.parse(localStorage.filter);
+      this.setState({list: currentFilterList});
+    }.bind(this));
+  },
+  componentWillUnmount: function() {
+    PubSub.unsubscribe(reloadFilterEvt);
+  },
+  handleDeleteFilter: function(filter) {
+    var deleteIndex;
+    var currentFilter = JSON.parse(localStorage.filter);
+
+    for (var i = currentFilter.length - 1; i >= 0; i--) {
+        if (currentFilter[i].id == filter.id) {
+            deleteIndex = i;
+            break;
+        }
+    }
+
+    currentFilter.splice(i,1);
+    localStorage.filter=JSON.stringify(currentFilter);
+    this.setState({list: currentFilter});
+  },
+  render: function() {
+    var rows = [];
+    this.state.list.forEach(function(item) {
+      rows.push(<FilterRow item={item} onDeleted={this.handleDeleteFilter} />);
+    }.bind(this));
+    return (
+    <section>
+        <h2>Current filters:</h2>
+        <div className="row">
+          <div className="col-lg-6">
+          <table className="table table-bordered" id="filter">
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>id</th>
+                    <th>actions</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
+          </div>
+          </div>
+      </section>
+    );
+  }
+});
+
+var NewFilter = React.createClass({
+  onSubmit: function(event) {
+    event.preventDefault();
+    var nameNode = this.refs.name.getDOMNode();
+    var idNode = this.refs.id.getDOMNode();
+    var name = nameNode.value.trim();
+    var id = idNode.value.trim();
+
+    if (name != "" && id != "") {
+        var currentFilter = JSON.parse(localStorage.filter);
+        currentFilter.push({name: name, id:id, lastModifyDate:"1"});
+        localStorage.filter = JSON.stringify(currentFilter);
+        PubSub.publish(reloadFilterEvt);
+    }
+    nameNode.value = "";
+    idNode.value = "";
+  },
+  render: function() {
+    var inputStyle = {width:'200px'};
+    return (
+    <form className="form-inline" onSubmit={this.onSubmit}>
+      Add filter:<br/>
+      <div className="form-group">
+        <input type="text" ref="name" className="form-control" style={inputStyle} placeholder="Name"/>
+      </div>
+      <div className="form-group">
+        <input type="text" ref="id" className="form-control" style={inputStyle} placeholder="ID"/>
+      </div>
+      <button className="btn btn-primary" type="submit" >Add</button>
+    </form>
+    );
+  }
+});
+
+var OptionsBox = React.createClass({
+  handleChange: function(event) {
+    localStorage.frequency = event.target.value;
+  },
+  render: function() {
+    var freq = localStorage.frequency;
+    return (
+      <div id="options">
+        <h2>Options</h2>
+          <div className="row">
+          <div className="col-lg-9 form-inline">
+          Check new defects every
+        <select className="form-control" defaultValue={freq} onChange={this.handleChange}>
+          <option>10</option>
+          <option>30</option>
+          <option>60</option>
+        </select>
+        second(s).<br/>
+        </div>
+        </div>
+          <CurrentFilters />
+          <NewFilter />
+      </div>
+    );
+  }
+});
+
+
+var Header = React.createClass({
+  render: function() {
+    return (
+    <div className="page-header">
+        <h1>
+            <img src={'64.png'} alt="Toast"/>
+                RTC Notification
+        </h1>
+    </div>
+    );
+  }
+});
+
+var NewDefectRow = React.createClass({
+  render: function() {
+    var link = item_url + this.props.item.id;
+    return (
+      <li>
+        <a href={link} target="_blank">{this.props.item.id} - {this.props.item.summary}</a>
+      </li>
+    );
+  }
+});
+
+var TodayItems = React.createClass({
+  getInitialState: function() {
+    return {
+      list: []
+    };
+  },
+  componentWillMount: function() {
+    var message = {
+      type: "initItems"
+    };
+
+    chrome.runtime.sendMessage(message, function(items) {
+      if (items) {
+        this.setState({list:items});
+      }
+    }.bind(this));
+  },
+  componentDidMount: function() {
+    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+      if(message.type == "addNewItem"){
+        console.log('add new item');
+        var list = this.state.list;
+        list.push(message.value);
+        this.setState({list:list});
+      }
+    }.bind(this));
+  },
+  render: function() {
+    var rows = [];
+    console.log(this.state.list);
+    this.state.list.forEach(function(item) {
+      rows.push(<NewDefectRow item={item} />);
+    }.bind(this));
+    return (
+      <div id="todayItems">
+        <h2>Defects since last check:</h2>
+        <ol>{rows}</ol>
+      </div>
+    );
+  }
+});
+
+var FocusingOnRow = React.createClass({
+  handleClick: function() {
+    this.props.onDeleted(this.props.item);
+  },
+  render: function() {
+    var link = item_url + this.props.item.id;
+    return (
+      <li>
+        <button onClick={this.handleClick} itemid={this.props.item.id} className="delete_focusing btn btn-danger">X</button>
+        <a href={link} target="_blank">{this.props.item.summary}</a>
+      </li>
+    );
+  }
+});
+
+var FocusingOn = React.createClass({
+  getInitialState: function() {
+    var focusingOnList = JSON.parse(localStorage.focusingOn);
+    return {
+      list: focusingOnList
+    };
+  },
+  handleDeleteFocusing: function(item) {
+    var focusingOnList = this.state.list
+    for (var i = focusingOnList.length - 1; i >= 0; i--) {
+        if (focusingOnList[i].id == item.id) {
+            deleteIndex = i;
+            break;
+        }
+    }
+
+    focusingOnList.splice(i,1);
+    localStorage.focusingOn=JSON.stringify(focusingOnList);
+    this.setState({list: focusingOnList});
+  },
+  componentDidMount: function() {
+    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+      if(message.type == "addFocusingOn"){
+        var focusingOnList = JSON.parse(localStorage.focusingOn);
+        self.setState({list: focusingOnList});
+      }
+    }.bind(this));
+  },
+  render: function() {
+    var rows = [];
+    this.state.list.forEach(function(item) {
+      rows.push(<FocusingOnRow item={item} onDeleted={this.handleDeleteFocusing} />);
+    }.bind(this));
+    return (
+      <div id="focusingOn">
+        <h2>Following:</h2>
+        <ol>{rows}</ol>
+      </div>
+    );
+  }
+});
+
+var Options = React.createClass({
+  render: function() {
+    var focusingOnList = JSON.parse(localStorage.focusingOn);
+    return (
+      <div>
+        <Header />
+        <OptionsBox />
+        <FocusingOn />
+        <TodayItems />
+      </div>
+    );
+  }
+});
+
+
+
+React.render(
+  <Options/>,
+  document.getElementById('content')
+);
