c2ee62df65f0c4194e26ed4d02865891be54fde8
jymsy <jym20083617@163.com>
2016-06-18 18:59:00 +0800

    update

---
+++ source/js/dispatcher/rtcDispatcher.js
@@ -0,0 +1,10 @@
+/*
+ *
+ * AppDispatcher
+ *
+ * A singleton that operates as the central hub for application updates.
+ */
+
+var Dispatcher = require('flux').Dispatcher;
+
+module.exports = new Dispatcher();
